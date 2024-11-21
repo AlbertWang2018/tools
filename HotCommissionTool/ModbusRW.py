805c478cc8b3497f7ddf35788e12f4bd73b3cb3b
Albert.Wang <36392874+AlbertWang2018@users.noreply.github.com>
2024-11-21 18:33:17 +0800

    Add files via upload

--- HotCommissionTool/ModbusRW.py
+++ HotCommissionTool/ModbusRW.py
@@ -11,20 +11,29 @@ read: ModbusWriteData.exe read 127.0.0.1 502 896 20
 write: ModbusWriteData.exe write 127.0.0.1 502 896 1
 '''
 
-try:    
+try:
     if len(argv)<5: 
         print(usage)
     else:
         mode = argv[1]
         host = argv[2]
         port = argv[3]
-        reg = int(argv[4])  # 896,Beat Heart
-        value = int(argv[5])
+        if len(argv)>5:
+            value = int(argv[5])
         client = ModbusTcpClient(host,port)
-        if argv[1]=='read': 
-            print(client.read_holding_registers(address=reg,count=value).registers)
-        if argv[1]=='write': 
+        if mode=='write': 
+            reg = int(argv[4])  # 896,Beat Heart
             client.write_register(address=reg,value=value)
-            print("Write success")
+            print("Write success")        
+        if mode=='read': 
+            if ',' in str(argv[4]):
+                res=[]
+                for reg in argv[4].split(','): 
+                    res.append(str(client.read_holding_registers(address=int(reg),count=1).registers).replace('[','').replace(']','').replace(' ',''))
+                print(res)
+            else:
+                reg = int(argv[4])
+                print(str(client.read_holding_registers(address=int(reg),count=value).registers).replace('[','').replace(']','').replace(' ',''))
+        client.close()
 except:
     print("Connect fail")

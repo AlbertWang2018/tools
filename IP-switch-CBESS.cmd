01fee8e41c3f11989f94956d9c1b1e7a30515d9d
Albert.Wang <36392874+AlbertWang2018@users.noreply.github.com>
2025-08-28 06:07:21 +0800

    Delete IP-switch-CBESS.cmd

--- IP-switch-CBESS.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p n=Please input power group No.:
-for /f "usebackq delims=" %%i in (`powershell -Command "Get-NetAdapter | Where-Object { $_.Status -eq 'Up' -and ($_.Name -like '*Ethernet*' -or $_.Name -like '*以太网*') } | Select-Object -ExpandProperty Name"`) do (
-    set "nic=%%i"
-    goto :found
-)
-:found
-netsh interface ip set address name="%nic%" source=static addr=10.149.3%n%.250 mask=255.255.255.0 gateway=10.149.3%n%.1 gwmetric=99
